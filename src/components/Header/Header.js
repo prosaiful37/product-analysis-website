9450187593c1948da148b6c3647791167fd2e507
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-04-05 15:26:33 +0600

    home page style setting

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,8 +9,8 @@ const Header = () => {
                 <a className="navbar-brand" href="/">
                     <img className='img-fulid logo-img ' src="logos.png" alt="" />
                 </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"      data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"     aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0 fs-5">
